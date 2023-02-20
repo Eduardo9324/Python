d980e1853da4a893657cd7b75445f7db3a0fcb67
Eduardo Toro <97927299+Eduardo9324@users.noreply.github.com>
2023-02-20 09:30:09 -0500

    mad-libs

---
+++ projects/mad-libs/index.py
@@ -0,0 +1,19 @@
+# Practica concatenar caracteres
+
+# Nombres
+nombre_uno = input("Ingresa un nombre: ")
+nombre_dos = input("Ingresa un nombre: ")
+nombre_tres = input("Ingresa un nombre: ")
+nombre_cuatro = input("Ingresa un nombre: ")
+
+# Verbos
+verbo_uno = input("Ingresa primer verbo: ")
+verbo_dos = input("Ingresa segundo verbo: ")
+verbo_tres = input("Ingresa tercer verbo: ")
+
+
+""" mad_lib = f"A {nombre} la gusta salir a {verbo_uno}, se siente muy bien siempre que lo hace, y en las noches suele {verbo_dos}, dice que le ayuda a relajar el cuerpo, no le gusta {verbo_tres} y su mama siempre lo obliga a hacerlo. " """
+
+mad_lib = f"Una vez {nombre_uno} descubrió un misterioso objeto dorado en la playa. Inmediatamente, {nombre_dos} supo que este objeto tenía un gran poder y decidió investigar su origen; Después de {verbo_uno} durante horas, {nombre_tres} encontró un mensaje oculto en el objeto que decía que solo el elegido podría manejar su poder. {nombre_dos} sabía que este mensaje era para él y decidió llevar el objeto a su aldea; Allí, {nombre_cuatro} descubrió que había un antiguo mito sobre un héroe que podría salvar al mundo de la oscuridad usando el objeto dorado. Determinado a cumplir su destino, {nombre_dos} se puso en camino para encontrar a este héroe; Después de {verbo_dos} durante semanas, {nombre_dos} finalmente encontró al héroe. Juntos, {nombre_tres} y el héroe se adentraron en la oscuridad para luchar contra las fuerzas del mal y salvar al mundo; Durante su aventura, {nombre_uno} y el héroe enfrentaron muchos desafíos y peligros, pero nunca perdieron la esperanza. Al final, gracias a su valentía y determinación, lograron derrotar al malvado villano y salvar al mundo; Después de {verbo_tres} de regreso a casa, {nombre_cuatro} se dio cuenta de que nunca habría sido capaz de hacerlo solo y agradeció al héroe por su ayuda. Ahora, {nombre_tres} era conocido en su aldea como un héroe en sí mismo y nunca olvidaría la aventura que cambió su vida para siempre."
+
+print(mad_lib)
